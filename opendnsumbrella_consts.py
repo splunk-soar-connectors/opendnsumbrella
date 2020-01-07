0f8a624eb7909df81cb03b055c559bdb2dda62db
Ricardo Munoz <rmunoz@rmunoz-mbp-9d822.sv.splunk.com>
2020-01-07 13:27:14 -0800

    pipeline fixes

--- opendnsumbrella_consts.py
+++ opendnsumbrella_consts.py
@@ -1,16 +1,8 @@
-# --
-# File: opendnsumbrella_connector.py
+# File: opendnsumbrella_consts.py
+# Copyright (c) 2014-2020 Splunk Inc.
 #
-# Copyright (c) Phantom Cyber Corporation, 2014-2018
-#
-# This unpublished material is proprietary to Phantom Cyber.
-# All rights reserved. The methods and
-# techniques described herein are considered trade secrets
-# and/or confidential. Reproduction or distribution, in whole
-# or in part, is forbidden except by express written permission
-# of Phantom Cyber.
-#
-# --
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
 
 
 OPENDNSUMB_JSON_DOMAIN = "domain"
